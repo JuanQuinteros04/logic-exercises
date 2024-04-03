47e8decad25039d28d897b4f504e9f01721ca9af
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-04-02 22:30:04 -0300

    Ejercicio 4.

--- src/main/java/com/example/ejercicioslogica/Ejercicios/AreaCirculo.java
+++ src/main/java/com/example/ejercicioslogica/Ejercicios/AreaCirculo.java
@@ -1,5 +1,6 @@
 package com.example.ejercicioslogica.Ejercicios;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 import static java.lang.Math.PI;
@@ -9,8 +10,9 @@ public class AreaCirculo {
     public void areaCirculo(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el radio del circulo: ");
-        double radio = Double.parseDouble(scanner.nextLine());
-        double area = PI * Math.pow(radio,2);
+        scanner.useLocale(Locale.US);
+        double radio = scanner.nextDouble();
+        double area = Math.PI * Math.pow(radio, 2);
         System.out.println("El area del circulo es: " + area);
     }
 }
