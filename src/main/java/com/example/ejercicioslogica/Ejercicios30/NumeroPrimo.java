99ce7b976bb253d044476cedc1f76a3bed71096c
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-10-29 23:02:40 -0300

    Cousin

--- src/main/java/com/example/ejercicioslogica/Ejercicios30/NumeroPrimo.java
+++ src/main/java/com/example/ejercicioslogica/Ejercicios30/NumeroPrimo.java
@@ -1,22 +1,30 @@
 package com.example.ejercicioslogica.Ejercicios30;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class NumeroPrimo {
-    Double numero = 0.00;
-    Scanner scanner = new Scanner(System.in);
-    Double resultado = 0.0;
+   Integer numeroUser = 0;
 
+   Scanner scanner = new Scanner(System.in);
+
+   List<Double> numerosDivisibles = new ArrayList<>();
     public void esPrimo(){
-        System.out.println("Ingrese un numero: ");
-        numero = scanner.nextDouble();
-        if (numero <= 1){
-            System.out.println("El numero no es primo");
+        System.out.println("Ingrese un numero mayor a 1: ");
+        numeroUser = scanner.nextInt();
+        Integer numeroDividido = Math.sqrt(numeroUser);
+        for (Double i = 1.0; i <= numeroDividido; i++){
+            if(numeroDividido % i == 0 ){
+                numerosDivisibles.add(i);
+            }
+            System.out.println(numeroDividido % i);
+            System.out.println(numeroDividido);
+            System.out.println(numeroDividido);
         }
-        for (int i = 2; numero%i != 0; i++){
-            if (i == numero){
-                System.out.println("El numero " + numero + " es primo.");
-            }continue;
+        System.out.println(numerosDivisibles);
+        if(numerosDivisibles.size() == 2){
+            System.out.println(numeroUser + " es primo.");
         }
     }
 }
